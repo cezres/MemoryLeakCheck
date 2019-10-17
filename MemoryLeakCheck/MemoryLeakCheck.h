c1bf5df3c78d691ddcdb7080e41e0b2e3b875846
cezres <cezres@163.com>
2019-10-17 21:21:17 +0800

    Init project

---
+++ MemoryLeakCheck/MemoryLeakCheck.h
@@ -0,0 +1,19 @@
+//
+//  MemoryLeakCheck.h
+//  MemoryLeakCheck
+//
+//  Created by 翟泉 on 2019/10/17.
+//  Copyright © 2019 cezres. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MemoryLeakCheck.
+FOUNDATION_EXPORT double MemoryLeakCheckVersionNumber;
+
+//! Project version string for MemoryLeakCheck.
+FOUNDATION_EXPORT const unsigned char MemoryLeakCheckVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MemoryLeakCheck/PublicHeader.h>
+
+
---
+++ MemoryLeakCheckTests/MemoryLeakCheckTests.swift
@@ -0,0 +1,33 @@
+//
+//  MemoryLeakCheckTests.swift
+//  MemoryLeakCheckTests
+//
+//  Created by 翟泉 on 2019/10/17.
+//  Copyright © 2019 cezres. All rights reserved.
+//
+
+import XCTest
+
+class MemoryLeakCheckTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
